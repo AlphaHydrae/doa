b7bee45d25fced597ada53a9156dcc0ff12d62db
Simon Oulevay <simon.oulevay@gmail.com>
2014-04-25 17:41:36 +0200

    Express app with jade and stylus.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,8 @@
-
 module.exports = function(grunt) {
 
   grunt.initConfig({
-
-    pkg: grunt.file.readJSON('package.json'),
-
-    jshint: {
-      all: [ 'index.js', 'lib/**/*.js', '!lib/assets/js/vendor/**', 'spec/**/*.js', '!spec/browser/support/angular*.js' ]
-    },
-
-    jasmine_node: {
-      projectRoot: 'spec/server',
-      requirejs: false,
-      forceExit: true,
-      growl: true
-    },
-
-    karma: {
-
-      options: {
-        configFile: 'karma.conf.js',
-        singleRun: true
-      },
-
-      dev: {
-      },
-
-      ci: {
-        browsers: [ 'PhantomJS' ]
-      }
-    },
-
-    watch: {
-
-      browser: {
-        files: [ 'lib/assets/js/**/*.js', 'lib/assets/js/**/*.coffee', 'spec/browser/**/*.js' ],
-        tasks: [ 'karma:ci' ],
-        options: {
-          atBegin: true
-        }
-      },
-
-      server: {
-        files: [ 'lib/**/*.js', '!lib/assets/**', 'spec/server/**/*.js' ],
-        tasks: [ 'jasmine_node' ],
-        options: {
-          atBegin: true
-        }
-      }
-    }
+    pkg: grunt.file.readJSON('package.json')
   });
 
-  grunt.loadNpmTasks('grunt-bump');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jasmine-node');
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('default', [ 'jshint', 'karma:ci', 'jasmine_node' ]);
+  grunt.registerTask('default', []);
 };
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 **Node.js periodic tasks monitoring tool.**
 
-[![NPM version](https://badge.fury.io/js/doa.png)](http://badge.fury.io/js/doa)
-[![Dependency Status](https://gemnasium.com/AlphaHydrae/doa.png)](https://gemnasium.com/AlphaHydrae/doa)
-[![Build Status](https://secure.travis-ci.org/AlphaHydrae/doa.png)](http://travis-ci.org/AlphaHydrae/doa)
+[![NPM version](https://badge.fury.io/js/doa.svg)](http://badge.fury.io/js/doa)
+[![Dependency Status](https://gemnasium.com/AlphaHydrae/doa.svg)](https://gemnasium.com/AlphaHydrae/doa)
+[![Build Status](https://travis-ci.org/AlphaHydrae/doa.svg?branch=master)](https://travis-ci.org/AlphaHydrae/doa)
 
 ## Meta
 
