33bc5ed92ec466546b1435701577f52068bac45a
Simon Oulevay <simon.oulevay@gmail.com>
2014-04-29 21:56:04 +0200

    Watch update and ping.

--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -57,20 +57,60 @@ app.controller('WatchFormController', [ '$scope', 'WatchManager', 'ServerSocket'
   });
 } ]);
 
-app.controller('WatchController', [ '$scope', function($scope) {
+app.controller('WatchController', [ '$scope', 'ServerSocket', 'WatchManager', function($scope, serverSocket, watchManager) {
 
-  $scope.statusClass = 'panel-info';
-  $scope.collapseClass = 'in';
-  if ($scope.watch.status == 'up') {
-    $scope.statusClass = 'panel-success';
-    $scope.collapseClass = '';
-  } else if ($scope.watch.status == 'down') {
-    $scope.statusClass = 'panel-danger';
-    $scope.collapseClass = '';
+  function reset() {
+    $scope.editing = false;
+    $scope.watchUpdate = _.clone($scope.watch);
   }
+
+  reset();
+
+  serverSocket.forward([ 'updated', 'update:error' ], $scope);
+
+  $scope.$on('socket:updated', function(ev, watch) {
+    if (watch.id == $scope.watch.id) {
+      reset();
+    }
+  });
+
+  function updateStatus() {
+    $scope.statusClass = 'panel-info';
+    $scope.collapseClass = 'in';
+    if ($scope.watch.status == 'up') {
+      $scope.statusClass = 'panel-success';
+      $scope.collapseClass = '';
+    } else if ($scope.watch.status == 'down') {
+      $scope.statusClass = 'panel-danger';
+      $scope.collapseClass = '';
+    }
+  }
+
+  updateStatus();
+  $scope.$watch('watch.status', updateStatus);
+  
 
   $scope.getWatchUrl = function() {
     return $scope.watch.url;
+  };
+
+  $scope.toggleEditing = function() {
+    if ($scope.editing) {
+      $scope.editing = false;
+      reset();
+    } else {
+      $scope.editing = true;
+    }
+  };
+
+  $scope.update = function() {
+    watchManager.update($scope.watchUpdate);
+  };
+
+  $scope.destroy = function() {
+    if (confirm('Are you sure you want to delete the "' + $scope.watch.name + '" watch?')) {
+      watchManager.delete($scope.watch.id);
+    }
   };
 } ]);
 
@@ -81,7 +121,7 @@ app.factory('WatchManager', [ '$rootScope', '$q', 'ServerSocket', function($root
         watches: []
       };
 
-  serverSocket.forward([ 'init', 'created' ]);
+  serverSocket.forward([ 'init', 'created', 'updated', 'pinged', 'deleted' ]);
 
   $rootScope.$on('socket:init', function (ev, data) {
     service.watches = data.watches;
@@ -92,12 +132,35 @@ app.factory('WatchManager', [ '$rootScope', '$q', 'ServerSocket', function($root
     service.watches.unshift(data);
   });
 
+  $rootScope.$on('socket:updated', function(ev, data) {
+    var watch = _.findWhere(service.watches, { id: data.id });
+    _.extend(watch, data);
+  });
+
+  $rootScope.$on('socket:pinged', function(ev, data) {
+    _.extend(_.findWhere(service.watches, { id: data.id }), data);
+  });
+
+  $rootScope.$on('socket:deleted', function(ev, id) {
+    var watch = _.findWhere(service.watches, { id: id });
+    service.watches.splice(_.indexOf(service.watches, watch), 1);
+  });
+
   service.init = function() {
     serverSocket.emit('init');
   };
 
   service.create = function(watch) {
     serverSocket.emit('create', watch);
+  };
+
+  service.update = function(watch) {
+    // TODO: don't do anything if there are no changes
+    serverSocket.emit('update', watch);
+  };
+
+  service.delete = function(id) {
+    serverSocket.emit('delete', id);
   };
 
   return service;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
-var express = require('express');
+var db = require('../lib/db'),
+    express = require('express'),
+    redis = require('redis');
 
-var router = express.Router();
+var client = redis.createClient(),
+    router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'doa' });
+});
+
+router.post(/^\/([a-z0-9]{5})$/, function(req, res) {
+
+  var id = req.params[0];
+  db.pingWatch(id, function(err, updates) {
+    if (err) {
+      return res.json(500, err.message);
+    }
+
+    client.publish('doa:watches', JSON.stringify({ name: 'pinged', data: updates }));
+
+    res.send(204);
+  });
 });
 
 module.exports = router;
